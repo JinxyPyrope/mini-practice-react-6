b0d5405d8a158967057351479892f10d7110a0b4
Nhoj <nhojplummer@yahoo.com>
2023-04-12 12:36:00 -0400

    Progress on practice

--- practice-6/src/App.js
+++ practice-6/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css"
+import { List } from "./components"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <List />
+    </div>
+  )
 }
 
 export default App
---
+++ practice-6/src/components/List.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function List(props) {
+  return <div>Hello World</div>
+}
+
+export default List
